b1fc01b3a9147ff9ced0f5894913f11008f40fb5
Prudhvi Raj Patnala <pruthvipatnala@gmail.com>
2022-07-21 18:40:26 -0700

    initial routing changes

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <h1>TODO: Add app summary </h1>
+      <h2>TODO: Add buttons to "Add" or "Edit" door</h2>
+      <Footer />
+    </>
+  )
+}
+
+export default Home;
